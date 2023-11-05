2f7ad1c1e5c0bc1c37ba0332ca3cc25e80b9a498
KleepyHunter <149455403+KleepyHunter@users.noreply.github.com>
2023-11-05 23:40:50 +0700

    Add files via upload
    Convention update!

--- advisor.py
+++ advisor.py
@@ -1,4 +1,4 @@
-#written by Kleecon
+# Written by Kleecon
 
 from piece import Piece
 
@@ -8,25 +8,25 @@ class Advisor(Piece):
         # Movement
         admissible_moves = []
 
-        # possible goal positions
+        # Possible goal positions
         x_orient = [1,1,-1,-1]
         y_orient = [1,-1,-1,1]
         maximum_move_count = 4
 
-        # iteration through all positions
+        # Iteration through all positions
         for cnt in range(maximum_move_count):
 
             # Possible position setting
             pos = (self.position[0] + x_orient[cnt], self.position[1] + y_orient[cnt])
 
             # Checkment
-            if self.get_piece_team_on_position(pos) != self.team.value:
-                if self.is_position_in_palace(pos):
-                    admissible_moves.append(pos)
+            if not self.is_position_teammate(pos)\
+            and self.is_position_in_palace(pos):
+                admissible_moves.append(pos)
 
-        # return
+        # Return
         return admissible_moves
 
 if __name__ == "__main__":
-    #test the class here Mortdog
+    # Test the class here Mortdog
     pass
