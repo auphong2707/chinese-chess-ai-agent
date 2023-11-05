12654e6a72378607097b1a47ae3c01b33bd984d9
Veil <auphong2707@gmail.com>
2023-11-05 19:12:05 +0700

    Update piece.py
    Fix the pylint

--- piece.py
+++ piece.py
@@ -4,7 +4,8 @@ from team import Team
 
 
 class Piece(ABC):
-    '''This class is an abtract class which is inherited by every pieces in the game'''
+    """This class is an abtract class which is inherited by every pieces in the game"""
+
     # [BEGIN CONSTANTS]
     # Board size
     BOARD_SIZE_X = 10
@@ -28,7 +29,7 @@ class Piece(ABC):
     # .position
     @property
     def position(self) -> tuple:
-        '''Getter of the position property, return the position of the piece'''
+        """Getter of the position property, return the position of the piece"""
         return self._position
 
     @position.setter
@@ -41,7 +42,7 @@ class Piece(ABC):
     # .admissible moves
     @property
     def admissible_moves(self) -> list:
-        '''Getter of the admissible_moves property, return the list of admissible move of a piece'''
+        """Getter of the admissible_moves property, return the list of admissible move of a piece"""
         return self.admissible_moves
 
     @admissible_moves.setter
@@ -53,35 +54,35 @@ class Piece(ABC):
     # [BEGIN METHODS]
     # Instance method
     def _get_piece_team_on_position(self, position: tuple) -> Team:
-        '''Return the team of the piece on the position (Team.NONE, Team.RED, Team.BLACK)'''
+        """Return the team of the piece on the position (Team.NONE, Team.RED, Team.BLACK)"""
         if self.is_position_on_board(position) is False:
             raise ValueError("The position is out of range")
 
         return self._board[position[0]][position[1]]
-    
+
     def is_position_teammate(self, position: tuple):
-        '''Return True if the piece on the position is teammate piece, vice versa'''
+        """Return True if the piece on the position is teammate piece, vice versa"""
         return self._get_piece_team_on_position(position) is self.team
 
     def is_position_free(self, position: tuple):
-        '''Return True if there is no piece on the position, vice versa'''
+        """Return True if there is no piece on the position, vice versa"""
         return self._get_piece_team_on_position(position) is Team.NONE
 
     def is_position_opponent(self, position: tuple):
-        '''Return True if the piece on the position is opponent piece, vice versa'''
+        """Return True if the piece on the position is opponent piece, vice versa"""
         return self._get_piece_team_on_position(position).value == -self.team.value
 
     # Abstract method
     @abstractmethod
     def get_admissible_moves(self) -> list:
-        '''Abstract method that return the list of admissible moves of a piece.
-        This method is used to initialize the piece'''
+        """Abstract method that return the list of admissible moves of a piece.
+        This method is used to initialize the piece"""
         pass
 
     # Static method
     @staticmethod
     def is_position_on_board(position: tuple) -> bool:
-        '''Return True if the position is a valid position of the board, vice versa'''
+        """Return True if the position is a valid position of the board, vice versa"""
         # Check if x component is on the board
         result_x = position[0] >= 0 and position[0] < Piece.BOARD_SIZE_X
 
@@ -92,7 +93,7 @@ class Piece(ABC):
 
     @staticmethod
     def is_position_in_palace(position: tuple) -> bool:
-        '''Return True if the position is in the palace, vice versa'''
+        """Return True if the position is in the palace, vice versa"""
         # Check if x component is in the palace
         result_x = (
             position[0] >= Piece.BOUND_PALACE_X_BLACK[0]
