61801caa5cd2ac9943eb81fee0409b536b7c62e1
Veil <auphong2707@gmail.com>
2023-11-06 08:05:12 +0700

    Update pawn.py
    Fix the pawn

--- pawn.py
+++ pawn.py
@@ -1,7 +1,8 @@
-#written by Kleecon
+# written by Kleecon
 
 from piece import Piece
 from team import Team
+
 
 class Pawn(Piece):
     def __init__(self, position: tuple, init_team: Team) -> None:
@@ -10,38 +11,36 @@ class Pawn(Piece):
 
     @property
     def _has_crossed_river(self):
-        if self._has_crossed_river == True:
+        if self._has_crossed_river is True:
             return True
-        
-        self._has_crossed_river = abs(self.position[0] + 9*(self.team.value-1)/2) < 5
+
+        self._has_crossed_river = (
+            abs(self.position[0] + 9 * (self.team.value - 1) / 2) < 5
+        )
 
     # Searching admissible moves for the pawn
     def get_admissible_moves(self):
-
         possible_moves = []
-        
+
         # Movement
         pos = (self.position[0] - self.team.value, self.position[1])
-        if self.is_position_on_board(pos)\
-        and not self.is_position_teammate(pos):
+        if self.is_position_on_board(pos) and not self.is_position_teammate(pos):
             possible_moves.append(pos)
-        
-        if self._has_crossed_river == True:
 
+        if self._has_crossed_river is True:
             new_pos = (self.position[0], self.position[1] + 1)
-            if self.is_position_on_board(pos)\
-            and not self.is_position_teammate(pos):
+            if self.is_position_on_board(pos) and not self.is_position_teammate(pos):
                 possible_moves.append(new_pos)
 
             new_pos = (self.position[0], self.position[1] - 1)
-            if self.is_position_on_board(pos)\
-            and not self.is_position_teammate(pos):
+            if self.is_position_on_board(pos) and not self.is_position_teammate(pos):
                 possible_moves.append(new_pos)
 
         # Capture (it's the same with movement, dang it)
 
         return possible_moves
 
+
 if __name__ == "__main__":
-    #test the class here Mortdog
+    # test the class here Mortdog
     pass
