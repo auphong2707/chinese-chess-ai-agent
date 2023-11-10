ba2eb4c9ceb35d594ba4556d472cf053125228f1
KleepyHunter <gargaggr@gmail.com>
2023-11-10 09:54:05 +0700

    Update node.py
    Update property UCT

--- node.py
+++ node.py
@@ -20,3 +20,21 @@ class Node:
         self.parent_iters_until_now = 0
     # [END INITIALIZATION]
 
+    # [Properties]
+    @property
+    def uct(self) -> float:
+        """This gets the UCT index of the current node"""
+
+        # Case 1: This node hasn't been iterated
+        if self.iters_until_now == 0:
+            return 0
+
+        # Case 2: This node has been iterated before
+        exploitation_component = self.win_until_now/self.iters_until_now
+        exploration_component = sqrt(log(self.parent_iters_until_now)/self.iters_until_now)
+
+        return exploitation_component + self.EXPLORATION_PARAMETER*exploration_component
+    # [End properties]
+
+    # [METHOD]
+    # Instance methods
