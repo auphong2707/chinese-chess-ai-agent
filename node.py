347791137a0b7a2b9d6386459143fbfce77ffda9
TheSyx <minhquantsy@gmail.com>
2023-11-15 21:53:23 +0700

    creating class NodeMCTS and some basic things on it, checking the TODO list

--- node.py
+++ node.py
@@ -1,4 +1,5 @@
 from cmath import inf
+from collections import defaultdict
 from math import sqrt, log
 from abc import ABC, abstractmethod
 from random import randint
@@ -62,8 +63,8 @@ class NodeMinimax(Node):
         # Minimax statistics
         self._alpha = -inf
         self._beta = inf
-        self._minimax_value = None
-        self._depth = None
+        self.minimax_value = None
+        self.depth = None
 
     # [END INITIALIZATION]
 
@@ -90,7 +91,7 @@ class NodeMinimax(Node):
         best_children, best_value = list(), 0
 
         for child in self.list_of_children:
-            evaluation_value = child.minimax_value*team.value
+            evaluation_value = child.minimax_value * team.value
             # If we found a new best value, then update it and reset the list
             if evaluation_value > best_value:
                 best_value = evaluation_value
@@ -102,3 +103,47 @@ class NodeMinimax(Node):
 
         # Return a random child among the best
         return best_children[randint(0, len(best_children) - 1)]
+
+
+class NodeMCTS(Node):
+    """This class represents a "Monte-Carlo tree search's node" in game tree"""
+
+    # [INITIALIZATION]
+    def __init__(self, game_state: GameState, parent, parent_move: tuple) -> None:
+        # Reference to a node
+        super().__init__(game_state, parent, parent_move)
+
+        # MCTS statistics
+        self._result = defaultdict(int)
+        self._number_of_visits = None
+        self._result[1] = 0
+        self._result[-1] = 0
+        self.list_of_unvisited_child = list()
+
+    # Properties initialization
+    @property
+    def q(self):
+        """Return the difference between wins and losses"""
+        return self._result[1] - self._result[-1]
+
+    @property
+    def n(self):
+        """Return the number of visits of this node"""
+        return self._number_of_visits
+
+    @property
+    def is_fully_expanded(self):
+        """Return whether it has been fully expanded or not"""
+        return len(self.list_of_unvisited_child) == 0
+
+    # [END INITIALIZATION]
+
+    # [METHOD]
+    # Instance method
+    def generate_all_unvisited_node(self):
+        """This method generates all unvisited nodes"""
+        self.list_of_unvisited_child = self.get_all_children()
+        return None
+
+    def _create_node(self, game_state: GameState, parent, parent_move: tuple):
+        return NodeMCTS(game_state, parent, parent_move)
