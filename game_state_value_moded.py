0db4a3e79460932fe2697c72cf5fe389751ee22b
whatsoever025 <149892454+whatsoever025@users.noreply.github.com>
2023-11-22 21:27:20 +0700

    ok

---
+++ game_state_value_moded.py
@@ -0,0 +1,361 @@
+# Made by: Veil
+import time
+from cmath import inf
+from copy import deepcopy
+from random import randint
+from piece import General, Advisor, Elephant, Chariot, Cannon, Horse, Pawn
+from team import Team
+
+
+class GameState:
+    """This class respresents the state of game containing
+    information and transforming method"""
+
+    # [BEGIN CONSTANTS]
+    # Board size
+    BOARD_SIZE_X = 10
+    BOARD_SIZE_Y = 9
+
+    # [BEGIN INITILIZATION]
+    def __init__(
+        self,
+        pieces_list_current: list,
+        pieces_list_opponent: list,
+        board: tuple,
+        current_team: Team,
+    ) -> None:
+        # Add the chess pieces to the list
+        self.pieces_list_current = pieces_list_current
+        self.pieces_list_opponent = pieces_list_opponent
+
+        # Declare read-only properties
+        self._value = None
+        self._board = board
+        self._current_team = current_team
+        self._all_child_gamestates = None
+
+    # Properties initialization
+    # .value
+    @property
+    def value(self) -> float:
+        """This is the Getter function of the value property,
+        return the value of the game state using chess pieces value"""
+
+        if self._value is None:
+            self._value = self._get_game_state_value()
+
+        return self._value
+
+    # .all_child_gamestates
+    @property
+    def all_child_gamestates(self) -> list:
+        """This is the Getter function of the list of child game states"""
+
+        if self._all_child_gamestates is None:
+            self._all_child_gamestates = self.generate_all_game_states()
+
+        return self._all_child_gamestates
+    # [END INITILIZATION]
+
+    # [BEGIN METHOD]
+    # Instance method
+    def _get_game_state_value(self):
+        """Return the evaluation value of the board"""
+
+        if self.get_team_win is Team.RED:
+            return inf
+
+        if self.get_team_win is Team.BLACK:
+            return -inf
+
+        current_value = 0
+        for piece in self.pieces_list_current + self.pieces_list_opponent:
+            current_value = current_value + piece.piece_value*piece.team.value
+
+        return current_value
+
+    def _get_the_opponent_team(self):
+        """This method will return the opponent team in this game state"""
+        if self._current_team is Team.BLACK:
+            return Team.RED
+        else:
+            return Team.BLACK
+
+    def _create_a_new_board(self, old_pos, new_pos):
+        """This method returns a new board after a piece moved"""
+        # Create a deepcopy of a new board
+        new_board = deepcopy(self._board)
+
+        # Transform the new board to list
+        new_board = list(map(list, new_board))
+
+        # Assign new value to the old position and new position of the moved piece
+        new_board[old_pos[0]][old_pos[1]] = Team.NONE
+        new_board[new_pos[0]][new_pos[1]] = self._current_team
+
+        # Transform the new board to tuple again
+        new_board = tuple(map(tuple, new_board))
+
+        # Return the answer
+        return new_board
+
+    def generate_game_state_with_move(self, old_pos, new_pos):
+        """This method creates a game state with a move
+        (return None if the game state is invalid)"""
+        # Create a copy of current board and transform it
+        new_board = self._create_a_new_board(old_pos, new_pos)
+
+        # Get the opponent team
+        opponent = self._get_the_opponent_team()
+
+        # Create new chess piece list and current team's general position
+        new_pieces_list_current, new_pieces_list_opponent = list(), list()
+        current_team_general_position = None
+
+        # Add the current team's pieces to the list
+        for piece in self.pieces_list_current:
+            # If the piece is the moved one
+            if piece.position == old_pos:
+                # Create a copy of that piece and update the statistic
+                new_piece = piece.create_copy()
+                new_piece.position = new_pos
+                new_piece.set_admissible_moves(new_board)
+
+                # Append new piece to the list
+                new_pieces_list_opponent.append(new_piece)
+
+            # Otherwise, append the reference to the list
+            else:
+                new_pieces_list_opponent.append(piece)
+
+            if isinstance(new_pieces_list_opponent[-1], General):
+                current_team_general_position = new_pieces_list_opponent[-1].position
+
+        # Add the opponent's pieces to the list
+        for piece in self.pieces_list_opponent:
+            # If the piece is eliminated then continue
+            if piece.position == new_pos:
+                continue
+
+            # Create a copy of the piece and update the statistic
+            new_piece = piece.create_copy()
+            new_piece.set_admissible_moves(new_board)
+
+            # Append new piece to the list
+            new_pieces_list_current.append(new_piece)
+
+        # Check if the game state is valid
+        # Iterate for each piece in the list and detect the invalid
+        for piece in new_pieces_list_current:
+            # If the piece is advisor or elephant, then skip
+            if isinstance(piece, Advisor) or isinstance(piece, Elephant):
+                continue
+
+            # If the piece is general, check the y position
+            if (
+                isinstance(piece, General)
+                and piece.position[1] == current_team_general_position[1]
+            ):
+                x_min = piece.position[0] - opponent.value
+                x_max = current_team_general_position[0] - \
+                    self._current_team.value
+                if x_min > x_max:
+                    x_min, x_max = x_max, x_min
+
+                has_obstacle = False
+                for x in range(x_min, x_max + 1):
+                    if new_board[x][piece.position[1]] is not Team.NONE:
+                        has_obstacle = True
+                        break
+
+                if has_obstacle is False:
+                    return None
+                continue
+
+            # Else: Check if admissible moves of the piece containing the general position
+            if current_team_general_position in piece.admissible_moves:
+                return None
+
+        # Return the game state which has the new information
+        return GameState(
+            new_pieces_list_current, new_pieces_list_opponent, new_board, opponent
+        ), (
+            old_pos, new_pos
+        )
+
+    def generate_random_game_state(self):
+        """This method will generate another gamestate that can be tranformed
+        by current method using each move of the piece"""
+
+        # Return a random child game states of the current game states
+        return self.all_child_gamestates[randint(0, len(self.all_child_gamestates) - 1)]
+
+    def generate_all_game_states(self):
+        """This method will return the list of all states that can be accessed
+        from the current state by a single move"""
+
+        # Create a list that keeps track of all game states that can be generated.
+        game_states_available = list()
+
+        # Iterating through all moves
+        for pieces in self.pieces_list_current:
+            for move in pieces.admissible_moves:
+                # Get the old position and new position of the chosen piece
+                old_pos = pieces.position
+                new_pos = move
+
+                # Add new game state into the above list
+                game_state = self.generate_game_state_with_move(
+                    old_pos, new_pos)
+
+                if game_state is not None:
+                    game_states_available.append(game_state)
+
+        return game_states_available
+
+    def get_team_win(self):
+        """This method return the winning team"""
+
+        # If the current game state has child game states, then return Team.NONE
+        if len(self.all_child_gamestates) > 0:
+            return Team.NONE
+
+        # Return the opponent if current team has no admissible move
+        return self._get_the_opponent_team()
+
+    # Class method
+    @classmethod
+    def generate_initial_game_state(cls):
+        """This method creates the initial board"""
+
+        # Create a list of chess pieces
+        pieces_list_red, pieces_list_black = list(), list()
+
+        # Create a list to keep track of the team of pieces in chess_pieces
+        board = [
+            [Team.NONE for columns in range(GameState.BOARD_SIZE_Y)]
+            for rows in range(GameState.BOARD_SIZE_X)
+        ]
+
+        # Create pieces in the first row of black team
+        for columns in range(GameState.BOARD_SIZE_Y):
+            # Create black chariots
+            if columns == 0 or columns == 8:
+                black_piece = Chariot((0, columns), Team.BLACK)
+
+            # Create black horses
+            elif columns == 1 or columns == 7:
+                black_piece = Horse((0, columns), Team.BLACK)
+
+            # Create black elephants
+            elif columns == 2 or columns == 6:
+                black_piece = Elephant((0, columns), Team.BLACK)
+
+            # Create black advisors
+            elif columns == 3 or columns == 5:
+                black_piece = Advisor((0, columns), Team.BLACK)
+
+            # Create black general
+            else:
+                black_piece = General((0, columns), Team.BLACK)
+
+            # Put the pieces created into chess_pieces list
+            pieces_list_black.append(black_piece)
+
+            # Assign team of the pieces to its position
+            board[0][columns] = Team.BLACK
+
+        # Create black cannons
+        for columns in range(1, 8, 6):
+            black_cannon = Cannon((2, columns), Team.BLACK)
+
+            # Put the pieces created into chess_pieces list
+            pieces_list_black.append(black_cannon)
+
+            # Assign team of the pieces to its position
+            board[2][columns] = Team.BLACK
+
+        # Create black pawns
+        for columns in range(0, GameState.BOARD_SIZE_Y, 2):
+            black_pawn = Pawn((3, columns), Team.BLACK)
+
+            # Put the pieces created into chess_pieces list
+            pieces_list_black.append(black_pawn)
+
+            # Assign team of the pieces to its position
+            board[3][columns] = Team.BLACK
+
+        # Create pieces in the last row of red team
+        for columns in range(GameState.BOARD_SIZE_Y):
+            # Create red chariots
+            if columns == 0 or columns == 8:
+                red_piece = Chariot((9, columns), Team.RED)
+
+            # Create red horses
+            elif columns == 1 or columns == 7:
+                red_piece = Horse((9, columns), Team.RED)
+
+            # Create red elephants
+            elif columns == 2 or columns == 6:
+                red_piece = Elephant((9, columns), Team.RED)
+
+            # Create red advisors
+            elif columns == 3 or columns == 5:
+                red_piece = Advisor((9, columns), Team.RED)
+
+            # Create red general
+            else:
+                red_piece = General((9, columns), Team.RED)
+
+            # Put the pieces created into chess_pieces list
+            pieces_list_red.append(red_piece)
+
+            # Assign team of the pieces to its position
+            board[9][columns] = Team.RED
+
+        # Create red cannons
+        for columns in range(1, 8, 6):
+            red_cannon = Cannon((7, columns), Team.RED)
+
+            # Put the pieces created into chess_pieces list
+            pieces_list_red.append(red_cannon)
+
+            # Assign team of the pieces to its position
+            board[7][columns] = Team.RED
+
+        # Create red pawns
+        for columns in range(0, GameState.BOARD_SIZE_Y, 2):
+            red_pawn = Pawn((6, columns), Team.RED)
+
+            # Put the pieces created into chess_pieces list
+            pieces_list_red.append(red_pawn)
+
+            # Assign team of the pieces to its position
+            board[6][columns] = Team.RED
+
+        # Change type of board into tuple
+        board = tuple(map(tuple, board))
+
+        # Set all admissible moves of the red pieces
+        for piece in pieces_list_red:
+            piece.set_admissible_moves(board)
+
+        # return the initial board
+        return GameState(pieces_list_red, pieces_list_black, board, Team.RED)
+
+    # [END METHOD]
+
+
+if __name__ == '__main__':
+    queue = [GameState.generate_initial_game_state()]
+    for depth in range(1, 4):
+        start = time.time()
+
+        new_queue = list()
+        for game_state_ in queue:
+            for state, move_ in game_state_.all_child_gamestates:
+                new_queue.append(state)
+
+        queue = new_queue
+        end = time.time()
+        print(depth, len(queue), end - start)
---
+++ piece_value_moded.py
@@ -0,0 +1,432 @@
+# Edited by: Veil, Kleecon
+"""Module providing the property of abstract class and team members"""
+from abc import ABC, abstractmethod
+from team import Team
+
+
+class Piece(ABC):
+    """This class is an abtract class which is inherited by every pieces in the game"""
+
+    # [BEGIN CONSTANTS]
+
+    _piece_value = None
+    _piece_type = None
+
+    # Board size
+    BOARD_SIZE_X = 10
+    BOARD_SIZE_Y = 9
+
+    # Palace bound
+    BOUND_PALACE_X_RED = tuple((7, 9))
+    BOUND_PALACE_X_BLACK = tuple((0, 2))
+    BOUND_PALACE_Y = tuple((3, 5))
+    # [END CONSTANTS]
+
+    # [BEGIN INITILIZATION]
+    def __init__(self, position: tuple, team: Team) -> None:
+        # Create properties
+        self.position = position
+        self.team = team
+        self._has_crossed_river = False
+
+        self.admissible_moves = None
+
+    def __str__(self) -> str:
+        return str(self.team) + '_' + self._piece_type
+
+    # Properties initialization
+    # .position
+    @property
+    def position(self) -> tuple:
+        """Getter of the position property, return the position of the piece"""
+        return self._position
+
+    @position.setter
+    def position(self, new_position: tuple) -> None:
+        """Setter of the position property, recieve a position as a tuple"""
+        if self.is_position_on_board(new_position) is False:
+            raise ValueError("The position is out of range")
+
+        self._position = new_position
+
+    # .piece value
+    @property
+    def piece_value(self):
+        """This method return the value of the piece"""
+        return self._piece_value
+
+    @property
+    def has_crossed_river(self):
+        """Return True if the pawn is crossed the river"""
+        if self._has_crossed_river is True:
+            return True
+
+        self._has_crossed_river = (
+            abs(self.position[0] + 9 * (self.team.value - 1) / 2) < 5
+        )
+        return self._has_crossed_river
+    # [END INITILIZATION]
+
+    # [BEGIN METHODS]
+    # Instance method
+    def _get_piece_team_on_position(self, position: tuple, board: tuple) -> Team:
+        """Return the team of the piece on the position (Team.NONE, Team.RED, Team.BLACK)"""
+        if self.is_position_on_board(position) is False:
+            raise ValueError("The position is out of range")
+
+        return board[position[0]][position[1]]
+
+    def is_position_teammate(self, position: tuple, board: tuple):
+        """Return True if the piece on the position is teammate piece, vice versa"""
+        return self._get_piece_team_on_position(position, board) is self.team
+
+    def is_position_free(self, position: tuple, board: tuple):
+        """Return True if there is no piece on the position, vice versa"""
+        return self._get_piece_team_on_position(position, board) is Team.NONE
+
+    def is_position_opponent(self, position: tuple, board: tuple):
+        """Return True if the piece on the position is opponent piece, vice versa"""
+        return self._get_piece_team_on_position(position, board).value == -self.team.value
+
+    def set_admissible_moves(self, board: tuple):
+        """This method will set a new list of admissible moves of the piece"""
+        self.admissible_moves = self.get_admissible_moves(board)
+
+    # Abstract method
+    @abstractmethod
+    def get_admissible_moves(self, board: tuple) -> list:
+        """Abstract method that return the list of admissible moves of a piece.
+        This method is used to initialize the piece"""
+        pass
+
+    @abstractmethod
+    def create_copy(self):
+        """This return a copy of current piece with a new attached board"""
+        pass
+
+    # Static method
+    @staticmethod
+    def is_position_on_board(position: tuple) -> bool:
+        """Return True if the position is a valid position of the board, vice versa"""
+        # Check if x component is on the board
+        result_x = position[0] >= 0 and position[0] < Piece.BOARD_SIZE_X
+
+        # Check if y component is on the board
+        result_y = position[1] >= 0 and position[1] < Piece.BOARD_SIZE_Y
+
+        return result_x and result_y
+
+    @staticmethod
+    def is_position_in_palace(position: tuple) -> bool:
+        """Return True if the position is in the palace, vice versa"""
+        # Check if x component is in the palace
+        result_x = (
+            position[0] >= Piece.BOUND_PALACE_X_BLACK[0]
+            and position[0] <= Piece.BOUND_PALACE_X_BLACK[1]
+        ) or (
+            position[0] >= Piece.BOUND_PALACE_X_RED[0]
+            and position[0] <= Piece.BOUND_PALACE_X_RED[1]
+        )
+
+        # Check if y component is on the board
+        result_y = (
+            position[1] >= Piece.BOUND_PALACE_Y[0]
+            and position[1] <= Piece.BOUND_PALACE_Y[1]
+        )
+
+        return result_x and result_y
+
+    # [END METHODS]
+
+
+class Advisor(Piece):
+    """Class representing an advisor"""
+
+    _piece_value = 20
+    _piece_type = 'advisor'
+
+    def get_admissible_moves(self, board: tuple):
+        # Movement
+        admissible_moves = []
+
+        # Possible goal positions
+        x_orient = [1, 1, -1, -1]
+        y_orient = [1, -1, -1, 1]
+        maximum_move_count = 4
+
+        # Iteration through all positions
+        for cnt in range(maximum_move_count):
+            # Possible position setting
+            pos = (self.position[0] + x_orient[cnt],
+                   self.position[1] + y_orient[cnt])
+
+            # Checkment
+            if (
+                self.is_position_on_board(pos)
+                and not self.is_position_teammate(pos, board)
+                and self.is_position_in_palace(pos)
+            ):
+                admissible_moves.append(pos)
+
+        # Return
+        return admissible_moves
+
+    def create_copy(self):
+        return Advisor(self.position, self.team)
+
+
+class Cannon(Piece):
+    """Class representing a cannon"""
+
+    _piece_value = 45
+    _piece_type = 'cannon'
+
+    def get_admissible_moves(self, board: tuple) -> list:
+        x_direction = [1, -1, 0, 0]
+        y_direction = [0, 0, 1, -1]
+        admissible_moves = []
+
+        for direction in range(4):
+            piece_behind = 0
+
+            for steps in range(1, 10):
+                new_position = (self.position[0] + steps*x_direction[direction],
+                                self.position[1] + steps*y_direction[direction])
+
+                # Check if the new position is on the board
+                if self.is_position_on_board(new_position):
+
+                    # Check if there is any piece on the new position
+                    if self.is_position_free(new_position, board) is False:
+                        piece_behind += 1
+
+                        # If there is an enemy piece behind the piece in new position
+                        if piece_behind == 2 and self.is_position_opponent(new_position, board):
+
+                            admissible_moves.append(new_position)
+                            break
+
+                    elif piece_behind == 0:
+                        admissible_moves.append(new_position)
+
+        return admissible_moves
+
+    def create_copy(self):
+        return Cannon(self.position, self.team)
+
+
+class Chariot(Piece):
+    """Class representing a chariot"""
+
+    _piece_value = 90
+    _piece_type = 'chariot'
+
+    def get_admissible_moves(self, board: tuple) -> list:
+        x_direction = [1, -1, 0, 0]
+        y_direction = [0, 0, 1, -1]
+
+        admissible_moves = []
+
+        for direction in range(4):
+            for steps in range(1, 10):
+                new_position = (
+                    self.position[0] + steps * x_direction[direction],
+                    self.position[1] + steps * y_direction[direction],
+                )
+
+                # Check if the new position is on the board
+                if self.is_position_on_board(new_position):
+                    # Check if there is any piece on the new position
+                    if self.is_position_free(new_position, board) is False:
+                        # Check if the piece on the new position is on the enemy team
+                        if self.is_position_opponent(new_position, board):
+                            admissible_moves.append(new_position)
+                        break
+
+                    admissible_moves.append(new_position)
+
+        return admissible_moves
+
+    def create_copy(self):
+        return Chariot(self.position, self.team)
+
+
+class Elephant(Piece):
+    """Class representing an elephant"""
+
+    _piece_value = 25
+    _piece_type = 'elephant'
+
+    def _cross_river(self, position: tuple):
+        """Return True if the piece cross river, vice versa"""
+        if self.team is Team.RED and position[0] < 6:
+            return True
+        if self.team is Team.BLACK and position[0] > 5:
+            return True
+        return False
+
+    def get_admissible_moves(self, board: tuple):
+        admissible_moves = []
+
+        # Possible goal positions
+        x_direction = [2, 2, -2, -2]
+        y_direction = [2, -2, 2, -2]
+        maximum_move_count = 4
+
+        # Possible block positions
+        x_block = [1, 1, -1, -1]
+        y_block = [1, -1, 1, -1]
+
+        for direction in range(maximum_move_count):
+            new_pos = (
+                self.position[0] + x_direction[direction],
+                self.position[1] + y_direction[direction],
+            )
+
+            block_pos = (
+                self.position[0] + x_block[direction],
+                self.position[1] + y_block[direction],
+            )
+
+            # Check if all the conditions below met to add admissible moves for elephant piece
+            if (
+                self.is_position_on_board(new_pos)
+                and self.is_position_free(block_pos, board)
+                and not self._cross_river(new_pos)
+                and not self.is_position_teammate(new_pos, board)
+            ):
+                admissible_moves.append(new_pos)
+
+        return admissible_moves
+
+    def create_copy(self):
+        return Elephant(self.position, self.team)
+
+
+class General(Piece):
+    """Class representing a general"""
+
+    _piece_value = 0
+    _piece_type = 'general'
+
+    def get_admissible_moves(self, board: tuple) -> list:
+        x_direction = [1, -1, 0, 0]
+        y_direction = [0, 0, 1, -1]
+
+        admissible_moves = []
+
+        for direction in range(4):
+            new_position = (
+                self.position[0] + x_direction[direction],
+                self.position[1] + y_direction[direction],
+            )
+
+            # Check if the new position is in the palace
+            if self.is_position_in_palace(new_position):
+                # Check if there is any piece on the new position
+                if self.is_position_free(new_position, board):
+                    admissible_moves.append(new_position)
+
+                # Check if the piece on the new position is on the enemy team
+                elif self.is_position_opponent(new_position, board):
+                    admissible_moves.append(new_position)
+
+        return admissible_moves
+
+    def create_copy(self):
+        return General(self.position, self.team)
+
+
+class Pawn(Piece):
+    """Class representing a pawn"""
+
+    _piece_value = 10
+    _piece_type = 'pawn'
+
+    @property
+    def piece_value(self):
+        if self.has_crossed_river is True:
+            self._piece_value = 2
+        return self._piece_value
+
+    # Searching admissible moves for the pawn
+    def get_admissible_moves(self, board: tuple) -> list:
+        possible_moves = []
+
+        # Movement
+        new_pos = (self.position[0] - self.team.value, self.position[1])
+        if self.is_position_on_board(new_pos) and not self.is_position_teammate(new_pos, board):
+            possible_moves.append(new_pos)
+
+        if self.has_crossed_river is True:
+            new_pos = (self.position[0], self.position[1] + 1)
+            if self.is_position_on_board(new_pos) and not self.is_position_teammate(new_pos, board):
+                possible_moves.append(new_pos)
+
+            new_pos = (self.position[0], self.position[1] - 1)
+            if self.is_position_on_board(new_pos) and not self.is_position_teammate(new_pos, board):
+                possible_moves.append(new_pos)
+
+        # Capture (it's the same with movement, dang it)
+
+        return possible_moves
+
+    def create_copy(self):
+        return Pawn(self.position, self.team)
+
+
+class Horse(Piece):
+    """Class representing a horse"""
+
+    _piece_value = 40
+    _piece_type = 'horse'
+
+    @property
+    def piece_value(self):
+        if len(self.admissible_moves) == 1:
+            self._piece_type = 30
+        if len(self.admissible_moves) == 2:
+            self._piece_type = 35
+        if len(self.admissible_moves) == 5 or 6:
+            self._piece_type = 45
+        if len(self.admissible_moves) == 7 or 8:
+            self._piece_type = 50
+        return super().piece_value
+    def get_admissible_moves(self, board: tuple) -> list:
+
+        # Movement
+        admissible_moves = []
+
+        # Possible goal positions
+        x_orient = [2, 2, 1, -1, -2, -2, -1, 1]
+        y_orient = [1, -1, -2, -2, -1, 1, 2, 2]
+        maximum_move_count = 8
+
+        # Possible middle move positions
+        p_orient = [1, 0, -1, 0]
+        q_orient = [0, -1, 0, 1]
+
+        for cnt in range(maximum_move_count):
+
+            # Middle position
+            pos = (self.position[0] + p_orient[cnt//2],
+                   self.position[1] + q_orient[cnt//2])
+
+            # Check the middle position
+            if self.is_position_on_board(pos)\
+                    and self.is_position_free(pos, board):
+
+                # Goal position
+                pos = (self.position[0] + x_orient[cnt],
+                       self.position[1] + y_orient[cnt])
+
+                # Check the goal position
+                if self.is_position_on_board(pos)\
+                        and not self.is_position_teammate(pos, board):
+                    admissible_moves.append(pos)
+
+        # Return
+        return admissible_moves
+
+    def create_copy(self):
+        return Horse(self.position, self.team)
