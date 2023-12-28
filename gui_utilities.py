cb17550ef7234eee4a92d7ff9d90ca5072b5ee72
Veil <65228307+auphong2707@users.noreply.github.com>
2023-12-28 20:11:39 +0700

    Fix lint and update

--- gui_utilities.py
+++ gui_utilities.py
@@ -1,7 +1,9 @@
 import pygame
 
+
 class Button():
     """This class represents the Button in Pygame"""
+
     def __init__(self, image, pos, text_input, font, base_color, hovering_color):
         self.image = image
         self.x_pos = pos[0]
@@ -14,29 +16,33 @@ class Button():
             self.image = self.text
         self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
         self.text_rect = self.text.get_rect(center=(self.x_pos, self.y_pos))
-        
+
     def draw(self, screen):
         mouse_pos = pygame.mouse.get_pos()
-        
+
         self.change_color(mouse_pos)
         if self.image is not None:
             screen.blit(self.image, self.rect)
         screen.blit(self.text, self.text_rect)
-        
+
     def check_for_input(self, position):
         return (
             position[0] in range(self.rect.left, self.rect.right)
             and position[1] in range(self.rect.top, self.rect.bottom)
         )
-        
+
     def change_color(self, position):
         if self.check_for_input(position):
-            self.text = self.font.render(self.text_input, True, self.hovering_color)
+            self.text = self.font.render(
+                self.text_input, True, self.hovering_color)
         else:
-            self.text = self.font.render(self.text_input, True, self.base_color)
-            
+            self.text = self.font.render(
+                self.text_input, True, self.base_color)
+
+
 class DropDown():
     """This class represents the Dropdown list in the Pygame"""
+
     def __init__(self, color_menu, color_option, x, y, w, h, font, main, options):
         self.color_menu = color_menu
         self.color_option = color_option
@@ -49,22 +55,24 @@ class DropDown():
         self.active_option = -1
 
     def draw(self, screen):
-        pygame.draw.rect(screen, self.color_menu[self.menu_active], self.rect, 0)
+        pygame.draw.rect(
+            screen, self.color_menu[self.menu_active], self.rect, 0)
         msg = self.font.render(self.main, 1, "White")
-        screen.blit(msg, msg.get_rect(center = self.rect.center))
+        screen.blit(msg, msg.get_rect(center=self.rect.center))
 
         if self.draw_menu:
             for i, text in enumerate(self.options):
                 rect = self.rect.copy()
                 rect.y += (i+1) * self.rect.height
-                pygame.draw.rect(screen, self.color_option[1 if i == self.active_option else 0], rect, 0)
+                pygame.draw.rect(
+                    screen, self.color_option[1 if i == self.active_option else 0], rect, 0)
                 msg = self.font.render(text, 1, "White")
-                screen.blit(msg, msg.get_rect(center = rect.center))
+                screen.blit(msg, msg.get_rect(center=rect.center))
 
     def update(self, event_list):
         mpos = pygame.mouse.get_pos()
         self.menu_active = self.rect.collidepoint(mpos)
-        
+
         self.active_option = -1
         for i in range(len(self.options)):
             rect = self.rect.copy()
@@ -85,21 +93,23 @@ class DropDown():
                     return self.active_option
         return -1
 
+
 class InputBox:
     """This class represents the Input box in Pygame"""
+
     def __init__(self, x, y, w, h, font, color_inactive, color_active, text=''):
         self.x = x
         self.y = y
         self.rect = pygame.Rect(x - w/2, y - h/2, w, h)
-        
+
         self.color_inactive = color_inactive
         self.color_active = color_active
         self.color = self.color_inactive
-        
+
         self.font = font
         self.text = text
         self.txt_surface = self.font.render(text, True, self.color)
-        
+
         self.active = False
 
     def handle_event(self, event):
@@ -120,7 +130,8 @@ class InputBox:
                 elif event.unicode.isdigit():
                     self.text += event.unicode
                 # Re-render the text.
-                self.txt_surface = self.font.render(self.text, True, self.color_inactive)
+                self.txt_surface = self.font.render(
+                    self.text, True, self.color_inactive)
 
     def update(self):
         # Resize the box if the text is too long.
